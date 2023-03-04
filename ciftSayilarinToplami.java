1fc36b4166310674a84771e919fa26bac78ac5c6
Alperen YALVAÇ <77541391+alperenyalvac@users.noreply.github.com>
2023-03-04 09:40:04 +0300

    Create ciftSayilarinToplami.java

---
+++ ciftSayilarinToplami.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, toplam = 0;
+        do {
+            System.out.println("Sayı girin: ");
+            a = sc.nextInt();
+            if ((a % 2 == 0) && (a % 4 == 0)){
+                System.out.println("Sayı eklendi: " + a);
+                toplam += a;
+            }
+        } while (a % 2 == 0);
+        System.out.println(toplam);
+        }
+}
