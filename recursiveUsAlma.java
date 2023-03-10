6d0039dcbd96a28ae6e18b9072e75aad7e8cdfdc
Alperen YALVAÇ <77541391+alperenyalvac@users.noreply.github.com>
2023-03-11 00:28:01 +0300

    Create recursiveUsAlma.java

---
+++ recursiveUsAlma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    static int usAlma(int taban, int us) {
+        if (us == 0) {
+            return 1;
+        } else if (us == 1) {
+            return taban;
+        } else {
+            return taban * usAlma(taban, us-1);
+        }
+        
+    }
+    
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Taban değeri giri: ");
+        int taban = sc.nextInt();
+        System.out.print("Üs değeri girin: ");
+        int us = sc.nextInt();
+        System.out.println(usAlma(taban, us));
+    }
+}
