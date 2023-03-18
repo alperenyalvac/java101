fb98e2a5a1892b1ed2bec473e4321d533ab24571
Alperen YALVAÇ <77541391+alperenyalvac@users.noreply.github.com>
2023-03-18 17:18:22 +0300

    Add files via upload

---
+++ maaşHesaplayıcı/src/maaşhesaplayıcı/Main.java
@@ -0,0 +1,9 @@
+package maaşhesaplayıcı;
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Alperen", 2000, 50, 2002);
+        
+        employee.toStringP();
+    }
+    
+}
