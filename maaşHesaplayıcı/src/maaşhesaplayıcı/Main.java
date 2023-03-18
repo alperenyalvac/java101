24d5f9b17ba68186394723b4989223f1a26e417e
Alperen YALVAÇ <77541391+alperenyalvac@users.noreply.github.com>
2023-03-18 17:19:57 +0300

    Delete maaşHesaplayıcı directory

--- maaşHesaplayıcı/src/maaşhesaplayıcı/Main.java
+++
@@ -1,9 +0,0 @@
-package maaşhesaplayıcı;
-public class Main {
-    public static void main(String[] args) {
-        Employee employee = new Employee("Alperen", 2000, 50, 2002);
-        
-        employee.toStringP();
-    }
-    
-}
