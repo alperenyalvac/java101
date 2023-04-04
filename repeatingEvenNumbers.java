4c19d2d86d2b93ede20e114ee8ea9963517eb475
Alperen YALVAÇ <77541391+alperenyalvac@users.noreply.github.com>
2023-04-05 01:41:39 +0300

    Create repeatingEvenNumbers.java

---
+++ repeatingEvenNumbers.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+
+    static boolean isFind(int[] arr, int value) {
+        for (int i : arr) {
+            if (i == value) {
+                return true;
+            }
+        }
+        return false;
+    }
+    public static void main(String[] args) throws Exception {
+        int[] list = {3, 7, 3, 4, 4, 3, 2, 9, 10, 21, 34, 34, 1, 33, 9, 1};
+        int[] duplicate = new int [list.length];
+        int startIndex = 0;
+
+        for (int i = 0; i < list.length; i++) {
+            for (int j = 0; j < list.length; j++) {
+                if((i != j) && (list[i] == list[j])) {
+                    if (list[i] % 2 == 0) {
+                    if (!isFind(duplicate, list[i])) {
+                        duplicate[startIndex++] = list[i];
+                    }
+                    break;
+                }
+            }
+            }
+        }
+        for (int value : duplicate) {
+            if (value != 0) {
+                System.out.println(value);
+            }
+        }
+
+    }
+}
