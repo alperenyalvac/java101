39eceb47bac54ee278d33fb3ee9d5e7d7327f18e
Alperen YALVAÇ <77541391+alperenyalvac@users.noreply.github.com>
2023-04-05 11:45:30 +0300

    Create transpose.java

---
+++ transpose.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[][] matrix = { {1,2,3}, {4,5,6}, {7,8,9} };
+        int[][] transpose = new int[3][3];
+
+        System.out.println("Matrisimiz : ");
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("Matrisimizin transpozu : ");
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = 0; j < matrix[i].length; j++){
+                transpose[i][j] = matrix[j][i];
+                System.out.print(transpose[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
